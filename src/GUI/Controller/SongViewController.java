4a29f5e40f7b2057cbafe2c4fd41a2940f046042
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2022-11-29 09:50:55 +0100

    Buttons

--- src/GUI/Controller/SongViewController.java
+++ src/GUI/Controller/SongViewController.java
@@ -2,13 +2,46 @@ package GUI.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class SongViewController {
-    @FXML
-    private Label lbl;
+    /*
+    public Button btnEditSong;
+    public Button btnDeleteSong;
+    public Button btnRestartSong;
+    public Button btnSkipSong;
+    public Button btnAddPlaylist;
+    public Button btnEditPlaylist;
+    public Button btnDeletePlaylist;
 
-    public void clickBtn(ActionEvent actionEvent) {
-        lbl.setText("Huh?");
+    public void handleAddSong(ActionEvent actionEvent) {
     }
+
+    public void handleEditSong(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteSong(ActionEvent actionEvent) {
+    }
+
+    public void handleRestart(ActionEvent actionEvent) {
+    }
+
+    public void handleSkipSong(ActionEvent actionEvent) {
+    }
+
+    public void handleAddPlaylist(ActionEvent actionEvent) {
+    }
+
+    public void handleAddSongToPlaylist(ActionEvent actionEvent) {
+    }
+
+    public void handleEditPlaylist(ActionEvent actionEvent) {
+    }
+
+    public void handleDeletePlaylist(ActionEvent actionEvent) {
+    }
+
+    public void HandleMovePlaylistSongUp(ActionEvent actionEvent) {
+    }
+     */
 }
