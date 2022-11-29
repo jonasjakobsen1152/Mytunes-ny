c02a0ad5e2a4662696ead6a52421a7675a92b17a
oliverager <114471849+oliverager@users.noreply.github.com>
2022-11-29 11:48:44 +0100

    SongDetailsViewController

---
+++ src/GUI/Controller/SongDetailsViewController.java
@@ -0,0 +1,37 @@
+package GUI.Controller;
+
+import BE.Song;
+import GUI.Model.SongModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class SongDetailsViewController extends BaseController{
+    @FXML
+    private TextField txtTitle, txtArtist, txtCategory, txtSeconds;
+    private SongModel model;
+
+    public void handleUpdate(ActionEvent actionEvent) throws Exception{
+        String updatedTitle = txtTitle.getText();
+        String updatedArtist = txtArtist.getText();
+        String updatedCategory = txtCategory.getText();
+        int updatedSeconds = Integer.parseInt(txtSeconds.getText());
+        Song updatedSong = new Song(model.getSelectedSong().getId(),
+                updatedTitle, updatedArtist, updatedCategory, updatedSeconds);
+
+        model.updateSong(updatedSong);
+
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+    }
+    @Override
+    public void setup() {
+
+        model = getModel().getSongModel();
+
+        txtTitle.setText(model.getSelectedSong().getTitle());
+    }
+}
